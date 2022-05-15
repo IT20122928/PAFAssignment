55ab5796eab9201da35bac365b288cfc36ad105f
IT20122928 <88573340+IT20122928@users.noreply.github.com>
2022-05-15 21:22:55 +0530

    Card API Created

---
+++ PaymentManagement/src/cardService/CardAPI.java
@@ -0,0 +1,98 @@
+package cardService;
+
+import java.io.IOException;
+
+import java.util.Map;
+import java.util.HashMap; 
+import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CardAPI
+ */
+@WebServlet("/CardAPI")
+public class CardAPI extends HttpServlet {
+    cardDetails cardObj = new cardDetails();
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CardAPI() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String output = cardObj.insertcardDetails(
+				request.getParameter("cardNumber"),
+				request.getParameter("cardHolderName"),
+				request.getParameter("ExpirationDate"),
+				request.getParameter("vcode"));
+		response.getWriter().write(output); 
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Map paras = getParasMap(request); 
+		String output = cardObj.updatecardDetails(
+				paras.get("cardID").toString(),
+				paras.get("cardNumber").toString(), 
+				paras.get("bill_id").toString(), 
+				paras.get("amount").toString(), 
+				paras.get("date").toString()); 
+		response.getWriter().write(output);
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Map paras = getParasMap(request); 
+		String output = cardObj.deletecardDetails(paras.get("cardID").toString()); 
+		response.getWriter().write(output); 
+	
+	}
+
+	
+	// Convert request parameters to a Map
+			private static Map getParasMap(HttpServletRequest request) { 
+				Map<String, String> map = new HashMap<String, String>(); 
+				try { 
+					Scanner scanner = new Scanner(request.getInputStream(), "UTF-8"); 
+					String queryString = scanner.hasNext() ? 
+					scanner.useDelimiter("\\A").next() : ""; 
+					scanner.close(); 
+					String[] params = queryString.split("&"); 
+					for (String param : params) {
+						String[] p = param.split("="); 
+						map.put(p[0], p[1]); 
+					} 
+				} catch (Exception e) 
+				{ 
+				
+				} 
+				return map; 
+			}
+}
